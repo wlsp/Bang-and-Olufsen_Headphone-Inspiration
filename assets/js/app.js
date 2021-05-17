d565efdc26867184d2f565d749fe27a5e88f7666
wlsp <wlsp@outlook.de>
2021-05-17 19:53:37 +0200

    last?

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,3 +37,21 @@ function validateEmail(email) {
     return isEmpty(email) || re.test(email);
 }
 
+
+const modal = document.getElementById("newsLetter");
+const btn = document.getElementById("subscribe");
+const span = document.getElementsByClassName("close")[0];
+
+subscribe.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
